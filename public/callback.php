84f19a16be5405c2bc5b72d542a5b11576309933
Asami Doi <doiasami1219@gmail.com>
2017-01-11 00:58:35 +0000

    modify require_once

--- public/callback.php
+++ public/callback.php
@@ -1,5 +1,8 @@
 <?php
-require_once __DIR__ . "/vendor/autoload.php";
+require_once(__DIR__.'/vendor/autoload.php'); 
+
+// require_once __DIR__ . "/vendor/autoload.php";
+// A http status of the response was '500 Internal Server Error'
 
 define("CHANNEL_ACCESS_TOKEN", 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=');
 define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
