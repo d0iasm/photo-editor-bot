8da48b7a393cb470bd188965de25251b1ebb1551
Asami Doi <doiasami1219@gmail.com>
2017-01-11 00:30:35 +0000

    add callback.php

---
+++ public/callback.php
@@ -0,0 +1,45 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+
+define("CHANNEL_ACCESS_TOKEN", '<your channel token>');
+define("CHANNEL_SECRET", '<your channel secret>');
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
+
+// $input = file_get_contents("php://input");
+// $json = json_decode($input);
+// $event = $json->events[0];
+
+$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$body = file_get_contents("php://input");
+$events = $bot->parseEventRequest($body, $signature);
+
+foreach ($events as $event) {
+    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+        $reply_token = $event->getReplyToken();
+        $text = $event->getText();
+        $bot->replyText($reply_token, $text);
+    }
+}
+
+echo "OK";
+
+// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+// $response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+
+// $response = $bot->getMessageContent('<messageId>');
+// if ($response->isSucceeded()) {
+//     $tempfile = tmpfile();
+//     fwrite($tempfile, $response->getRawBody());
+// } else {
+//     error_log($response->getHTTPStatus() . ' ' . $response->getRawBody());
+// }
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+// $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+// syslog(LOG_EMERG, print_r($event->replyToken, true));
+// syslog(LOG_EMERG, print_r($response, true));
+
+?>
