0634eb4ce423b7c2a1e3a14df901018a057fe74c
Asami Doi <doiasami1219@gmail.com>
2017-01-11 00:44:08 +0000

    no use SDK

--- public/callback.php
+++ public/callback.php
@@ -1,15 +1,17 @@
 <?php
-// require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
 
-// define("CHANNEL_ACCESS_TOKEN", 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=');
-// define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
+define("CHANNEL_ACCESS_TOKEN", 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=');
+define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
 
 // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
 // $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
 
-// $input = file_get_contents("php://input");
-// $json = json_decode($input);
-// $event = $json->events[0];
+$input = file_get_contents("php://input");
+$json = json_decode($input);
+$event = $json->events[0];
+$type = $event->{"message"}->{"type"};
+$replyToken = $event->{"replyToken"};
 
 // $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 // $body = file_get_contents("php://input");
@@ -44,23 +46,10 @@
 
 
 
-
-$accessToken = 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=';
-
-//ユーザーからのメッセージ取得
-$json_string = file_get_contents('php://input');
-$jsonObj = json_decode($json_string);
-
-$type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
-
-//ReplyToken取得
-$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
-
-//メッセージ以外のときは何も返さず終了
 if($type != "text"){
 	exit;
 }else{
-    $text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
+    $text = $event->{"message"}->{"text"};
 }
 
 $response_format_text = [
@@ -81,7 +70,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json; charser=UTF-8',
-    'Authorization: Bearer ' . $accessToken
+    'Authorization: Bearer ' . CHANNEL_ACCESS_TOKEN
     ));
 $result = curl_exec($ch);
 curl_close($ch);
