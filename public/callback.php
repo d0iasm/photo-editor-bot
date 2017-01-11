bf0b3c8fe55295d720660626eb334442f1e9ef9a
Asami Doi <doiasami1219@gmail.com>
2017-01-11 00:49:49 +0000

    use accessToken variable

--- public/callback.php
+++ public/callback.php
@@ -7,6 +7,8 @@ define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
 // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
 // $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
 
+$accessToken = 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=';
+
 $input = file_get_contents("php://input");
 $json = json_decode($input);
 $event = $json->events[0];
@@ -70,10 +72,9 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json; charser=UTF-8',
-    'Authorization: Bearer ' . CHANNEL_ACCESS_TOKEN
+    'Authorization: Bearer ' . $accessToken
     ));
 $result = curl_exec($ch);
 curl_close($ch);
-
 
 ?>
