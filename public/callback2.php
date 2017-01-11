d6963aab9deedb793b569e28d291a9ba77663181
Asami Doi <doiasami1219@gmail.com>
2017-01-11 11:49:44 +0000

    not use callback.php

---
+++ public/callback2.php
@@ -0,0 +1,39 @@
+<?php
+// define("CHANNEL_ACCESS_TOKEN", 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=');
+// define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
+
+// $json_string = file_get_contents('php://input');
+// $jsonObj = json_decode($json_string);
+
+// $type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
+
+// $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+
+// if($type != "text"){
+// 	exit;
+// }else{
+//     $text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
+// }
+
+// $response_format_text = [
+//   "type" => "text",
+//   "text" => $text
+// ];
+
+// $post_data = [
+// 	"replyToken" => $replyToken,
+// 	"messages" => [$response_format_text]
+// ];
+
+// $ch = curl_init("https://api.line.me/v2/bot/message/reply");
+// curl_setopt($ch, CURLOPT_POST, true);
+// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//     'Content-Type: application/json; charser=UTF-8',
+//     'Authorization: Bearer ' . CHANNEL_ACCESS_TOKEN
+//     ));
+// $result = curl_exec($ch);
+// curl_close($ch);
+?>
