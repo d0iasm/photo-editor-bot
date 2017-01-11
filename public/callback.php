cc4a64c9d37b192af7073ed602c600c78aa37023
Asami Doi <doiasami1219@gmail.com>
2017-01-11 02:13:56 +0000

    modify response

--- public/callback.php
+++ public/callback.php
@@ -54,9 +54,20 @@ if ("message" == $event->type) {            //一般的なメッセージ(文字
 } else {
     //なにもしない
 }
-$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-syslog(LOG_EMERG, print_r($event->replyToken, true));
-syslog(LOG_EMERG, print_r($response, true));
-return;
+// $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+// syslog(LOG_EMERG, print_r($event->replyToken, true));
+// syslog(LOG_EMERG, print_r($response, true));
+
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($textMessageBuilder));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json; charser=UTF-8',
+    'Authorization: Bearer ' . CHANNEL_ACCESS_TOKEN
+    ));
+$result = curl_exec($ch);
+curl_close($ch);
 
 ?>
