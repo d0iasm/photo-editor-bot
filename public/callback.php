1d15c064bae17901d1695482fb5f5367fcdf80b8
Asami Doi <doiasami1219@gmail.com>
2017-01-11 00:37:03 +0000

    add index.php

--- public/callback.php
+++ public/callback.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 
-define("CHANNEL_ACCESS_TOKEN", '<your channel token>');
-define("CHANNEL_SECRET", '<your channel secret>');
+define("CHANNEL_ACCESS_TOKEN", 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=');
+define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
