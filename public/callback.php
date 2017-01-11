f956c5338cda8564b1864cb784a05cdde54372c4
Asami Doi <doiasami1219@gmail.com>
2017-01-11 02:08:44 +0000

    use SDK ver2

--- public/callback.php
+++ public/callback.php
@@ -13,21 +13,50 @@ define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
 // $signature = base64_encode($hash);
 // Compare X-Line-Signature request header string and the signature
 
+// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
+// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
+
+// $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+// $body = file_get_contents("php://input");
+// $events = $bot->parseEventRequest($body, $signature);
+
+// foreach ($events as $event) {
+//     if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+//         $reply_token = $event->getReplyToken();
+//         $text = $event->getText();
+//         $bot->replyText($reply_token, $text);
+//     }
+// }
+
+// echo "OK";
+
+$input = file_get_contents('php://input');
+$json = json_decode($input);
+$event = $json->events[0];
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
 
-$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$body = file_get_contents("php://input");
-$events = $bot->parseEventRequest($body, $signature);
-
-foreach ($events as $event) {
-    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
-        $reply_token = $event->getReplyToken();
-        $text = $event->getText();
-        $bot->replyText($reply_token, $text);
+//イベントタイプ判別
+if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
+    //テキストメッセージにはオウムで返す
+    if ("text" == $event->message->type) {
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+    } else {
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
     }
+} elseif ("follow" == $event->type) {        //お友達追加時
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("よろしくー");
+} elseif ("join" == $event->type) {           //グループに入ったときのイベント
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('こんにちは よろしくー');
+} elseif ('beacon' == $event->type) {         //Beaconイベント
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Godanがいんしたお(・∀・) ');
+} else {
+    //なにもしない
 }
-
-echo "OK";
+$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+syslog(LOG_EMERG, print_r($event->replyToken, true));
+syslog(LOG_EMERG, print_r($response, true));
+return;
 
 ?>
