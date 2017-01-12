1cf8bdf3389bd9987771a9f70a19402ed3280a33
Asami Doi <doiasami1219@gmail.com>
2017-01-12 02:24:34 +0000

    same line-bot-sdk-tiny

--- public/callback.php
+++ public/callback.php
@@ -2,22 +2,54 @@
 define("CHANNEL_ACCESS_TOKEN", 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=');
 define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
+// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
+// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
 
 
-$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$body = file_get_contents("php://input");
+// $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+// $body = file_get_contents("php://input");
 
-$events = $bot->parseEventRequest($body, $signature);
+// $events = $bot->parseEventRequest($body, $signature);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage($events[0]->replyToken, $textMessageBuilder);
-if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
-}
+// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+// $response = $bot->replyMessage($events[0]->replyToken, $textMessageBuilder);
+// if ($response->isSucceeded()) {
+//     echo 'Succeeded!';
+//     return;
+// }
 
-echo $response->getHTTPStatus . ' ' . $response->getRawBody();
+// echo $response->getHTTPStatus . ' ' . $response->getRawBody();
+
+$channelAccessToken = CHANNEL_ACCESS_TOKEN;
+$channelSecret = CHANNEL_SECRET;
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
+
 
 ?>
