b9c665a7810983a2174156b34388ec54a689df21
Asami Doi <doiasami1219@gmail.com>
2017-01-12 04:18:06 +0000

    create new client

--- public/callback.php
+++ public/callback.php
@@ -5,3 +5,5 @@ define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
 // require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once('./LINEBotTiny.php');
+
+$client = new LINEBotTiny(CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET);
