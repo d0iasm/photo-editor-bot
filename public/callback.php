a076d94e9e7ba2d14377915054787f0a0608c716
Asami Doi <doiasami1219@gmail.com>
2017-01-12 04:26:01 +0000

    get client event

--- public/callback.php
+++ public/callback.php
@@ -7,3 +7,10 @@ define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
 require_once('./LINEBotTiny.php');
 
 $client = new LINEBotTiny(CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET);
+$event = $client->parseEvents()[0];
+
+if($event['type'] == 'message'){
+    if($event['type']['text'] == 'text'){
+        
+    }
+}
