b0a6841ad884e50c7c4c0f836789c2061d7e7f5a
Asami Doi <doiasami1219@gmail.com>
2017-01-12 02:28:51 +0000

    add LINEBotTiny.php

--- public/callback.php
+++ public/callback.php
@@ -1,24 +1,8 @@
 <?php
+require_once('./LINEBotTiny.php');
+
 define("CHANNEL_ACCESS_TOKEN", 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=');
 define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
-
-// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
-// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
-
-
-// $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-// $body = file_get_contents("php://input");
-
-// $events = $bot->parseEventRequest($body, $signature);
-
-// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-// $response = $bot->replyMessage($events[0]->replyToken, $textMessageBuilder);
-// if ($response->isSucceeded()) {
-//     echo 'Succeeded!';
-//     return;
-// }
-
-// echo $response->getHTTPStatus . ' ' . $response->getRawBody();
 
 $channelAccessToken = CHANNEL_ACCESS_TOKEN;
 $channelSecret = CHANNEL_SECRET;
@@ -50,6 +34,5 @@ foreach ($client->parseEvents() as $event) {
             break;
     }
 };
-
 
 ?>
