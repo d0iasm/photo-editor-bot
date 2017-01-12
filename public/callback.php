2f485fe4f56b8787a4072842fcc32feb6b40aa50
Asami Doi <doiasami1219@gmail.com>
2017-01-12 04:34:20 +0000

    use SDK

--- public/callback.php
+++ public/callback.php
@@ -1,34 +1,47 @@
 <?php
-define("CHANNEL_ACCESS_TOKEN", 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=');
-define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
+// define("CHANNEL_ACCESS_TOKEN", 'CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=');
+// define("CHANNEL_SECRET", '776bcf263a10cf4cb30e1f2feeb33013');
 
-require_once('./LINEBotTiny.php');
+// require_once('./LINEBotTiny.php');
 
-$client = new LINEBotTiny(CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET);
+// $client = new LINEBotTiny(CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET);
 
-foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $message['text']
-                            )
-                        )
-                    ));
-                    break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log("Unsupporeted event type: " . $event['type']);
-            break;
-    }
-};
+// foreach ($client->parseEvents() as $event) {
+//     switch ($event['type']) {
+//         case 'message':
+//             $message = $event['message'];
+//             switch ($message['type']) {
+//                 case 'text':
+//                     $client->replyMessage(array(
+//                         'replyToken' => $event['replyToken'],
+//                         'messages' => array(
+//                             array(
+//                                 'type' => 'text',
+//                                 'text' => $message['text']
+//                             )
+//                         )
+//                     ));
+//                     break;
+//                 default:
+//                     error_log("Unsupporeted message type: " . $message['type']);
+//                     break;
+//             }
+//             break;
+//         default:
+//             error_log("Unsupporeted event type: " . $event['type']);
+//             break;
+//     }
+// };
+
+use LINE\LINEBot\EchoBot\Dependency;
+use LINE\LINEBot\EchoBot\Route;
+use LINE\LINEBot\EchoBot\Setting;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$setting = Setting::getSetting();
+$app = new Slim\App($setting);
+
+(new Dependency())->register($app);
+(new Route())->register($app);
+$app->run();
