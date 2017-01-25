04ea639ca6645b1ea5940751bb32f4ccfb18e8a5
Asami Doi <doiasami1219@gmail.com>
2017-01-25 05:19:18 +0000

    add callback.php file

--- public/index.php
+++ public/index.php
@@ -1,25 +1,9 @@
 <?php
-// use LINE\LINEBot\PhotoEditor\Dependency;
-// use LINE\LINEBot\PhotoEditor\Route;
-// use LINE\LINEBot\PhotoEditor\Setting;
+use LINE\LINEBot\PhotoEditor\Dependency;
+use LINE\LINEBot\PhotoEditor\Route;
+use LINE\LINEBot\PhotoEditor\Setting;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('CnOpazNl3Ns+DC9fXQckj97e0O4AAgWLZw1o6Gbym0xYMgl4gh4fIuf7k7ywc36LRCQ0gytM7hyBBepk1bfglDpgIqGO+aPlhfh3byhIi1yiqJ5vOjDs8l+hjYWhGVczYi4XIzsZYhDM1+W4y62jVwdB04t89/1O/w1cDnyilFU=');
-// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '776bcf263a10cf4cb30e1f2feeb33013']);
-
-// $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-// $body = file_get_contents("php://input");
-
-// $events = $bot->parseEventRequest($body, $signature);
-
-// $input = file_get_contents('php://input');
-// $json = json_decode($input);
-// $event = $json->events[0];
-
-// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-
-// $bot->replyMessage($event->replyToken, $textMessageBuilder);
 
 // $app = new \Slim\App;
 // $app->post('/add/example2', function () {
@@ -28,10 +12,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 // $app->run();
 
-// $setting = Setting::getSetting();
-// $app = new Slim\App($setting);
+$setting = Setting::getSetting();
+$app = new Slim\App($setting);
 
-// (new Dependency())->register($app);
-// (new Route())->register($app);
+(new Dependency())->register($app);
+(new Route())->register($app);
 
-// $app->run();
+$app->run();
