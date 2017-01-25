dd7e8b8cff40b355acf8fceca6f53686fab79cfe
Asami Doi <doiasami1219@gmail.com>
2017-01-25 02:46:20 +0000

    test Slim sample

--- public/index.php
+++ public/index.php
@@ -1,14 +1,25 @@
 <?php
-use LINE\LINEBot\PhotoEditor\Dependency;
-use LINE\LINEBot\PhotoEditor\Route;
-use LINE\LINEBot\PhotoEditor\Setting;
+// use LINE\LINEBot\PhotoEditor\Dependency;
+// use LINE\LINEBot\PhotoEditor\Route;
+// use LINE\LINEBot\PhotoEditor\Setting;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$setting = Setting::getSetting();
-$app = new Slim\App($setting);
+// $setting = Setting::getSetting();
+// $app = new Slim\App($setting);
 
-(new Dependency())->register($app);
-(new Route())->register($app);
+// (new Dependency())->register($app);
+// (new Route())->register($app);
+
+// $app->run();
+
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+]];
+$app = new \Slim\App($config);
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello " . $args['name']);
+});
 
 $app->run();
