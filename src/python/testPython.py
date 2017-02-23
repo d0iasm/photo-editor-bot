b0d26306b3a8dada27bbac2d4e976c350e5bd933
Asami Doi <doiasami1219@gmail.com>
2017-02-23 16:59:34 +0900

    return from python

--- src/python/testPython.py
+++ src/python/testPython.py
@@ -4,12 +4,15 @@ import cv2
 
 if __name__=='__main__':
 
+    print('fuga')
+    print('fugafuga')
+
     def editImage():
         # print ('parameter1 is' + sys.argv[1])
         # print ('parameter2 is' + sys.argv[2])
         print ('result is OK!')
         print ('result is NG!')
-        
+
         return 'hoge'
 
     print(editImage())
