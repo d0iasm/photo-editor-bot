1bd0517009d6ff026db31e50256032bb4d798473
Asami Doi <doiasami1219@gmail.com>
2017-02-23 10:31:59 +0900

    add python folder

---
+++ src/python/filter.py
@@ -0,0 +1,14 @@
+import os
+import cv2
+
+img = cv2.imread('../../images/camera.png', cv2.IMREAD_COLOR)
+
+imgHeight, imgWidth = img.shape[:2]
+size = (int(imgHeight/2), int(imgWidth/2))
+
+halfImg = cv2.resize(img, size)
+
+dirname = 'img'
+os.mkdir(dirname)
+
+cv2.imwrite(os.path.join(dirname, 'half.jpg'), halfImg)
