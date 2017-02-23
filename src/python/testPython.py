39970bd7b680ec97e10cdd863d69fa0d72602612
Asami Doi <doiasami1219@gmail.com>
2017-02-23 16:56:34 +0900

    return

--- src/python/testPython.py
+++ src/python/testPython.py
@@ -1,18 +1,15 @@
-# import os
+import os
 import sys
-# import cv2
-
-print('testPython.py実行')
+import cv2
 
 if __name__=='__main__':
 
-    print('testPython.pyの__main__')
-
     def editImage():
-        print('testPython.pyのeditImage()')
         # print ('parameter1 is' + sys.argv[1])
         # print ('parameter2 is' + sys.argv[2])
         print ('result is OK!')
         print ('result is NG!')
+        
+        return 'hoge'
 
-    editImage()
+    print(editImage())
