ad58a59e9975b5f11eadd7df91d19c1e1473b5a6
Asami Doi <doiasami1219@gmail.com>
2017-02-23 16:52:52 +0900

    add confirm msg

--- public/testPython.php
+++ public/testPython.php
@@ -5,6 +5,4 @@
   var_dump($fullPath);
   var_dump($outpara[0]);
   var_dump($outpara[1]);
-  var_dump($outpara[2]);
-  var_dump($outpara[3]);
   echo '<PRE>';
--- src/python/testPython.py
+++ src/python/testPython.py
@@ -1,6 +1,6 @@
-import os
+# import os
 import sys
-import cv2
+# import cv2
 
 print('testPython.py実行')
 
@@ -10,8 +10,8 @@ if __name__=='__main__':
 
     def editImage():
         print('testPython.pyのeditImage()')
-        print ('parameter1 is' + sys.argv[1])
-        print ('parameter2 is' + sys.argv[2])
+        # print ('parameter1 is' + sys.argv[1])
+        # print ('parameter2 is' + sys.argv[2])
         print ('result is OK!')
         print ('result is NG!')
 
