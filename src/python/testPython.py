5b16dfe4e4df0a091991d1ce9fa17270570b5e82
Asami Doi <doiasami1219@gmail.com>
2017-02-23 16:50:59 +0900

    add confirm msg

--- src/python/testPython.py
+++ src/python/testPython.py
@@ -2,9 +2,14 @@ import os
 import sys
 import cv2
 
+print('testPython.py実行')
+
 if __name__=='__main__':
 
+    print('testPython.pyの__main__')
+
     def editImage():
+        print('testPython.pyのeditImage()')
         print ('parameter1 is' + sys.argv[1])
         print ('parameter2 is' + sys.argv[2])
         print ('result is OK!')
