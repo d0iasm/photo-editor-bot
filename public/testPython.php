a45ce05543fe4b6eb390d5b894ebad3462db22cf
Asami Doi <doiasami1219@gmail.com>
2017-02-24 23:46:32 +0900

    test exec python

--- public/testPython.php
+++ public/testPython.php
@@ -1,20 +1,20 @@
 <?php
   echo('hoge');
-  // $fullPath ='/Users/DOIasami/.pyenv/shims/python ../src/python/testPython.py abcd 1234';
-  // exec($fullPath, $outpara);
-  // echo '<PRE>';
-  // var_dump($fullPath);
-  // var_dump($outpara[0]);
-  // var_dump($outpara[1]);
-  // var_dump($outpara[2]);
-  // var_dump($outpara[3]);
-  // echo '<PRE>';
+  $fullPath ='/Users/DOIasami/.pyenv/shims/python ../src/python/testPython.py abcd 1234';
+  system($fullPath, $outpara);
+  echo '<PRE>';
+  var_dump($fullPath);
+  var_dump($outpara[0]);
+  var_dump($outpara[1]);
+  var_dump($outpara[2]);
+  var_dump($outpara[3]);
+  echo '<PRE>';
 
-  $args = array_map('escapeshellarg', ['hoge', 'fuga']);
-  $cmd = vsprintf('python ../src/python/testPython.py %s %s 2>&1', $args);
-  system($cmd, $output, $status);
-  $output = implode($output);
-  var_dump(compact('output', 'status'));
+  // $args = array_map('escapeshellarg', ['hoge', 'fuga']);
+  // $cmd = vsprintf('python ../src/python/testPython.py %s %s 2>&1', $args);
+  // system($cmd, $output, $status);
+  // $output = implode($output);
+  // var_dump(compact('output', 'status'));
 
   // $fullPath ='python ../src/python/filter.py';
   // exec($fullPath, $outpara);
