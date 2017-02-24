b0d6f203ad614fad4d1227d9494a60326a2abc6b
Asami Doi <doiasami1219@gmail.com>
2017-02-24 21:25:39 +0900

    get image Object from S3

--- src/python/filter.py
+++ src/python/filter.py
@@ -1,32 +1,34 @@
 import os
 import sys
+import numpy
+from PIL import Image
 import cv2
 import boto3
 import botocore
 import s3Constant
+
+s3Constant = s3Constant.S3Constant()
+AWS_ACCESS_KEY_ID = s3Constant.getAwsAccessKeyId
+AWS_SECRET_ACCESS_KEY = s3Constant.getAwsSecretAccessKey
+S3_BUCKET = s3Constant.getS3Bucket
+
+s3client = boto3.Session(
+    aws_access_key_id = AWS_ACCESS_KEY_ID,
+    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+    region_name = 'ap-northeast-1'
+).client('s3')
 
 class Filter(object):
     """docstring for ."""
     # def __init__(self, arg):
         # self.arg = arg
 
-    s3Constant = s3Constant.S3Constant()
-    AWS_ACCESS_KEY_ID = s3Constant.getAwsAccessKeyId
-    AWS_SECRET_ACCESS_KEY = s3Constant.getAwsSecretAccessKey
-    S3_BUCKET = s3Constant.getS3Bucket
+    # s3 = boto3.resource('s3')
+    # bucket = s3.Bucket(S3_BUCKET)
 
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(S3_BUCKET)
+    # print(bucket.name)
 
-    print(bucket.name)
-
-    # session = boto3.Session(
-    #     aws_access_key_id = AWS_ACCESS_KEY_ID
-    #     aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-    #     region_name = 'ap-northeast-1'
-    # )
-
-    s3client = boto3.Session().client('s3')
+    # s3client = boto3.Session().client('s3')
 
     response = s3client.list_objects(
         Bucket = S3_BUCKET,
@@ -37,6 +39,18 @@ class Filter(object):
         keys = [content['Key'] for content in response['Contents']]
         print(keys)
 
+    data = s3client.get_object(Bucket=S3_BUCKET, Key='pome.jpg')
+    # print(data['Body'])
+    # body = data['Body'].read()
+    # print(type(body))
+    # print(data['Body'].read())
+    # infile = open(data['Body'].read(), 'rb')
+    # cv2.imencode(Binary(data['Body'].read()), cv2.IMREAD_COLOR)
+    # imgArray = numpy.asarray(data['Body'].read())
+    # data_np = numpy.array(data['Body'].read(), dtype=numpy.uint8)
+    img = numpy.asarray(bytearray(data['Body'].read()), dtype="uint8")
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+
     # rowImage = []
     # for key in keys:
     #     fp = StringIO()
@@ -46,23 +60,7 @@ class Filter(object):
     #     print (fp.getvalue())
     #     fp.close()
 
-
-    data = open('dest/half.jpg', 'rb')
-    s3.Bucket(S3_BUCKET).put_object(Key='test.jpg', Body=data)
-
-
-    # bucket = s3.Bucket(S3_BUCKET)
-    # exists = True
-    # try:
-    #     s3.meta.client.head_bucket(Bucket=S3_BUCKET)
-    # except botocore.exceptions.ClientError as e:
-    #     # If a client error is thrown, then check that it was a 404 error.
-    #     # If it was a 404 error, then the bucket does not exist.
-    #     error_code = int(e.response['Error']['Code'])
-    #     if error_code == 404:
-    #         exists = False
-
-    img = cv2.imread('../../images/camera.png', cv2.IMREAD_COLOR)
+    # img = cv2.imread('../../images/camera.png', cv2.IMREAD_COLOR)
 
     imgHeight, imgWidth = img.shape[:2]
     size = (int(imgHeight/2), int(imgWidth/2))
@@ -74,3 +72,12 @@ class Filter(object):
         os.mkdir(dirname)
 
     cv2.imwrite(os.path.join(dirname, 'half.jpg'), halfImg)
+
+    def send_edited_image():
+        data = open('dest/half.jpg', 'rb')
+        print(data)
+        # s3.Bucket(S3_BUCKET).put_object(Key='test.jpg', Body=data)
+        s3client.put_object(Bucket=S3_BUCKET, Key='test.jpg', Body=data)
+        data.close()
+
+    send_edited_image()
