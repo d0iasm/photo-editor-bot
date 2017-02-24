0c2147e5699baba604dd2fbe6464eaeefcae2d05
Asami Doi <doiasami1219@gmail.com>
2017-02-24 12:33:30 +0900

    modi

--- public/testAWS.php
+++ public/testAWS.php
@@ -13,6 +13,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
     try {
         // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
         $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+        $upload = $s3->upload($bucket, 'hogeFile.jpg', fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
 ?>
         <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
 <?php } catch(Exception $e) { ?>
