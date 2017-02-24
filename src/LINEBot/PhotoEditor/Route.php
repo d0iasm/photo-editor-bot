0beb8ba5f2377353afbe685d021a943da4bc93bc
Asami Doi <doiasami1219@gmail.com>
2017-02-24 13:18:17 +0900

    add fopen rb

--- src/LINEBot/PhotoEditor/Route.php
+++ src/LINEBot/PhotoEditor/Route.php
@@ -57,21 +57,21 @@ class Route
                     $s3 = \Aws\S3\S3Client::factory();
                     $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 
-                    $upload = $s3->upload($bucket, 'hoge.jpg', $binaryImage, 'public-read');
+                    $upload = $s3->upload($bucket, 'hoge.jpg', fopen($binaryImage, 'rb'), 'public-read');
 
                     $editedImage = new ImageMessageBuilder('https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/150x150.jpg', 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/150x150.jpg');
                     $bot->replyMessage($event->getReplyToken(), $editedImage);
 
-                    // try {
-                    //   $upload = $s3->upload($bucket, 'hoge.jpg', $binaryImage, 'public-read');
-                    //   // $upload = $s3->upload($bucket, 'hogeFile.jpg', fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
-                    //   // $upload->get('ObjectURL')
-                    //   $editedImage = new ImageMessageBuilder('https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/150x150.jpg', 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/150x150.jpg');
-                    //   $bot->replyMessage($event->getReplyToken(), $editedImage);
-                    // } catch(Exception $e) {
-                    //   $errorText = new TextMessageBuilder($e);
-                    //   $bot->replyMessage($event->getReplyToken(), $errorText);
-                    // }
+                    try {
+                      $upload = $s3->upload($bucket, 'hoge.jpg', $binaryImage, 'public-read');
+                      // $upload = $s3->upload($bucket, 'hogeFile.jpg', fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+                      // $upload->get('ObjectURL')
+                      $editedImage = new ImageMessageBuilder('https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/150x150.jpg', 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/150x150.jpg');
+                      $bot->replyMessage($event->getReplyToken(), $editedImage);
+                    } catch(Exception $e) {
+                      $errorText = new TextMessageBuilder($e);
+                      $bot->replyMessage($event->getReplyToken(), $errorText);
+                    }
 
                 }else if($event instanceof TextMessage) {
                     $getText = $event->getText();
