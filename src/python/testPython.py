6eee4db94e809654c4d52dcb0aafbc1c168400ed
Asami Doi <doiasami1219@gmail.com>
2017-02-25 06:54:40 +0900

    test exec python

--- src/python/testPython.py
+++ src/python/testPython.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append('/usr/local/Cellar/opencv3/3.2.0/lib/python2.7/site-packages')
-import cv2
+# import cv2
 
 def editImage():
     pass
@@ -9,7 +9,8 @@ def editImage():
 # if __name__=='__main__':
 
 print (sys.path)
+print (os.path.dirname(__file__))
 # print ('parameter1 is' + sys.argv[1])
-print ('parameter2 is' + sys.argv[2])
+# print ('parameter2 is' + sys.argv[2])
 print ('result is OK!')
 print ('result is NG!')
