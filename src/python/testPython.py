5559b585b020b8f7156743a82ce1c219ac4bc31b
Asami Doi <doiasami1219@gmail.com>
2017-02-25 06:39:39 +0900

    test exec python

--- src/python/testPython.py
+++ src/python/testPython.py
@@ -1,13 +1,14 @@
 import os
 import sys
-import cv2
+# import cv2
 
 def editImage():
     pass
 
 # if __name__=='__main__':
 
-print ('parameter1 is' + sys.argv[1])
+print ('sys.path')
+# print ('parameter1 is' + sys.argv[1])
 print ('parameter2 is' + sys.argv[2])
 print ('result is OK!')
 print ('result is NG!')
