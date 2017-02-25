beb75dddebc3aa290c7647f696f4a95d6a0aa3b7
Asami Doi <doiasami1219@gmail.com>
2017-02-25 23:50:41 +0900

    use []

--- src/LINEBot/PhotoEditor/Route.php
+++ src/LINEBot/PhotoEditor/Route.php
@@ -138,7 +138,8 @@ class Route {
                 }else if($event instanceof TextMessage) {
                     $getText = $event->getText();
                     if(strpos($getText, '加工の調整をする') !== false){
-                      setFiltertype('edge');
+                      // setFiltertype('edge');
+                      $GLOBALS['filtertype'] = IMG_FILTER_EMBOSS;
                       $act1 = new MessageTemplateActionBuilder('labelHoge1', 'textHoge1');
                       $act2 = new MessageTemplateActionBuilder('labelHoge2', 'textHoge2');
                       $mono = new CarouselColumnTemplateBuilder('モノクロ', 'mono', 'https://s3-us-west-2.amazonaws.com/lineapitest/hamburger_240.jpeg', [$act1, $act2]);
