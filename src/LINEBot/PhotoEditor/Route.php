71114719bdadb9e8bd00d65e2cacdfc24a453168
Asami Doi <doiasami1219@gmail.com>
2017-02-25 23:15:21 +0900

    add CaluselTemplateBuilder

--- src/LINEBot/PhotoEditor/Route.php
+++ src/LINEBot/PhotoEditor/Route.php
@@ -124,8 +124,8 @@ class Route {
                     if(strpos($getText, '加工の調整をする') !== false){
                       $act1 = new MessageTemplateActionBuilder('labelHoge1', 'textHoge1');
                       $act2 = new MessageTemplateActionBuilder('labelHoge2', 'textHoge2');
-                      $mono = new CarouselColumnTemplateBuilder('モノクロ', 'mono', 'https://s3-us-west-2.amazonaws.com/lineapitest/hamburger_240.jpeg', [$act1, $act2]);
-                      $mono2 = new CarouselColumnTemplateBuilder('モノクロ', 'mono', 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/mono2.jpg', [$act1, $act2]);
+                      $mono = new CarouselColumnTemplateBuilder('モノクロ', 'mono', 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/mono.jpg', [$act1, $act2]);
+                      $mono2 = new CarouselColumnTemplateBuilder('モノクロ', 'mono', 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/mono.jpg', [$act1, $act2]);
                       $mono3 = new CarouselColumnTemplateBuilder('モノクロ', 'mono', 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/mono.jpg', [$act1, $act2]);
                       $template = new CarouselTemplateBuilder([$mono, $mono2, $mono3]);
                       $templateMessage = new TemplateMessageBuilder('どんな加工にするか調整できます', $template);
