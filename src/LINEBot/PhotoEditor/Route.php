dbadf3c302150e219c54fbf01fbc1b131202b2d3
Asami Doi <doiasami1219@gmail.com>
2017-02-25 11:00:12 +0900

    get image size

--- src/LINEBot/PhotoEditor/Route.php
+++ src/LINEBot/PhotoEditor/Route.php
@@ -83,11 +83,11 @@ class Route
                         // $oldlink = 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/raw_image.jpg';
                         // $newlink = str_replace('https://', 'http://', $oldlink);
                         $size = getimagesize('http://placehold.jp/150x150.png');
-                        if ($size) {
-                          $imageSize = new TextMessageBuilder($size);
+                        if ($size == false) {
+                          $imageSize = new TextMessageBuilder('失敗');
                           $bot -> replyMessage($event->getReplyToken(), $imageSize);
                         }else{
-                          $imageSize = new TextMessageBuilder('失敗');
+                          $imageSize = new TextMessageBuilder($size);
                           $bot -> replyMessage($event->getReplyToken(), $imageSize);
                         }
 
