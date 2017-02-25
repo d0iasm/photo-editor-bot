a9d966bf53a9e91d9c3267ed8d6431a12a21f392
Asami Doi <doiasami1219@gmail.com>
2017-02-25 10:29:46 +0900

    get image size

--- src/LINEBot/PhotoEditor/Route.php
+++ src/LINEBot/PhotoEditor/Route.php
@@ -73,7 +73,9 @@ class Route
                         // $editedImage = new ImageMessageBuilder('https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/edited_image.jpg', 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/resized_image.jpg');
                         // $bot->replyMessage($event->getReplyToken(), $editedImage);
 
-                        $size = getimagesize('https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/raw_image.jpg');
+                        $oldlink = 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/raw_image.jpg';
+                        $newlink = str_replace('https://', 'http://', $oldlink);
+                        $size = getimagesize($newlink);
                         if ($size) {
                           $imageSize = new TextMessageBuilder($size);
                           $bot -> replyMessage($event->getReplyToken(), $imageSize);
