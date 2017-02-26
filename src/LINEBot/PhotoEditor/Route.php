1c4eb4241e8e141af9be3e6bbcef9368b9e6c112
Asami Doi <doiasami1219@gmail.com>
2017-02-26 14:14:01 +0900

    send data to s3

--- src/LINEBot/PhotoEditor/Route.php
+++ src/LINEBot/PhotoEditor/Route.php
@@ -103,7 +103,7 @@ class Route
                 }else if($event instanceof TextMessage) {
                     $getText = $event->getText();
                     if(strpos($getText, '加工の調整をする') !== false){
-                      $act1 = new MessageTemplateActionBuilder($editor->setNum(5), $editor->getNum());
+                      $act1 = new MessageTemplateActionBuilder('hoge', 'hoge');
                       $act2 = new MessageTemplateActionBuilder('emboss', 'emboss');
                       $mono = new CarouselColumnTemplateBuilder('mono', 'モノクロ画像にする', 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/mono.jpg', [$act1, $act2]);
                       $nega = new CarouselColumnTemplateBuilder('nega', '色を反転させる', 'https://s3-ap-northeast-1.amazonaws.com/photo-editor-bot/nega.jpg', [$act1, $act2]);
@@ -114,8 +114,9 @@ class Route
                       $templateMessage = new TemplateMessageBuilder('どんな加工にするか調整できます。', $template);
                       $bot->replyMessage($event->getReplyToken(), $templateMessage);
                     }else if(strpos($getText, 'emboss') !== false){
+                      $editor->setNum(5);
                       $replyText = new TextMessageBuilder($editor->getNum());
-                      // $replyText = new TextMessageBuilder('emboss 加工に変更しました');
+                      // $replyText = new TextMessageBuilder('emboss加工に変更しました');
                       $bot->replyMessage($event->getReplyToken(), $replyText);
                     }
                 }
